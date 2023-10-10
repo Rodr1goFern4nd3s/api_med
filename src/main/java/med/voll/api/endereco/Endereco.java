1219122412e109701000d2048e69ff824d598261
Rodrigo Fernandes Carvalho <rodrigo.fernandes@cgu.gov.br>
2023-10-10 19:25:22 -0300

    criado a interface repository e a tabela medicos

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import med.voll.api.medico.DadosCadastroMedico;
 
 @Embeddable
 @Getter
@@ -18,4 +19,14 @@ public class Endereco {
     private String cidade;
     private String uf;
     private String cep;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+        this.bairro = dados.bairro();
+        this.cidade = dados.cidade();
+        this.uf = dados.uf();
+        this.cep = dados.cep();
+    }
 }
