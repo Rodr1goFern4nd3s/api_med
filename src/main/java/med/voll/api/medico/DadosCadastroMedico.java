b135ce16b0c60d1e08a04235be95db6788dce25c
Rodrigo Fernandes Carvalho <rodrigo.fernandes@cgu.gov.br>
2023-10-11 19:02:41 -0300

    regras de validacao de campos aplicada nos atributos de medico

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,27 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String telefone, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+        @NotBlank
+        @Size(min = 8, max = 30)
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Size(max = 11)
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        Especialidade especialidade,
+        @NotBlank
+        @Valid
+        DadosEndereco endereco) {
 }
